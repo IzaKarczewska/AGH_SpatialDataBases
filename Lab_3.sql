f2dc84fbf96a1c289d78d3b1dd322e5fa6a62456
IzaKarczewska <izakarczewska1@wp.pl>
2024-11-04 18:33:16 +0100

    Lab3 final version

--- Lab_3.sql
+++ Lab_3.sql
@@ -69,3 +69,59 @@ from
 
 select * from streets_reprojected;
 
+-- Zad 4
+create table input_points (id serial, geometry geometry);
+insert into input_points (geometry) values
+	('POINT (8.36093 49.03174)'),
+	('POINT (8.39876 49.00644)');
+
+select * from input_points;
+
+-- Zad 5
+alter table input_points 
+alter column geometry type geometry(POINT, 3068)
+using st_setsrid(geometry, 3068);
+
+--select UpdateGeometrySRID('input_points','geometry', 3068);
+--select * from input_points;
+
+update input_points
+set geometry=st_setsrid(geometry,3068);
+
+-- Zad 6
+select * from street_2019;
+with reprojected as (
+	select 
+		gid, node_id, link_id, point_num, z_level, "intersect", lat, lon, st_setsrid(geom, 3068) as geom
+	from 
+		street_2019
+), bufor as(
+	select st_buffer(st_makeline(geometry), 0.002) as geom
+	from input_points
+)
+select reprojected.*
+from reprojected
+cross join bufor
+where st_contains(bufor.geom, reprojected.geom);
+
+-- Zad 7
+select * from poi_2019 where type='Sporting Goods Store';
+select * from land_use_a_2019;
+with buffer as(
+	select st_buffer(geom, 0.003) as geom
+	from land_use_a_2019
+	where type='Park (City/County)'
+)
+select count(1)
+from poi_2019
+cross join buffer
+where type='Sporting Goods Store' and st_contains(buffer.geom, poi_2019.geom);
+
+-- Zad 8
+create table T2019_KAR_BRIDGES as
+select st_intersection(r.geom, w.geom) as geom
+from railways_2019 as r
+join water_lines_2019 as w on st_intersects(r.geom, w.geom);
+
+
+
